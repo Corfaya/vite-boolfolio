8c026fad203035fa7d3e3b70d7aa02a8bab78894
Corfaya <lauracorrado0696@gmail.com>
2024-10-14 17:32:25 +0200

    [UPD] pagination added without fixing prev and next behaviour

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@ import ProjectCard from './components/ProjectCard.vue';
 export default {
   data() {
     return {
-      projects: []
+      projects: [],
+      current_page: null,
+      last_page: null,
+      first_page: 1
     }
   },
   components: {
@@ -16,7 +19,15 @@ export default {
   methods: {
     getProjects() {
       axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
-        this.projects = res.data.results;
+        this.projects = res.data.results.data;
+        this.current_page = res.data.results.current_page;
+        this.last_page = res.data.results.last_page;
+      })
+    },
+    changePage(p) {
+      this.current_page = p;
+      axios.get('http://127.0.0.1:8000/api/projects', {params: {page: p}}).then((res) => {
+        this.projects = res.data.results.data;
       })
     }
   }
@@ -33,6 +44,24 @@ export default {
       <div class="row gy-4">
         <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
       </div>
+
+      <nav aria-label="Page navigation" class="d-flex justify-content-center my-5">
+        <ul class="pagination">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous" @click="changePage(current_page - 1)">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item" v-for="page in last_page">
+            <a class="page-link" href="#" @click="changePage(page)"> {{ page }} </a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next" @click="changePage(current_page + 1)" >
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </main>
 </template>
