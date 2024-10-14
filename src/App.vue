20bc28d318431fa2fa9e1c1e38a6c59f8bc5a64e
Corfaya <lauracorrado0696@gmail.com>
2024-10-14 16:26:02 +0200

    [UPD]] Milestone 5: v-for using props to show multiples card projects

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,25 @@ export default {
     getProjects() {
       axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
         this.projects = res.data.results;
-        console.log(this.projects)
       })
     }
   }
 }
 </script>
 <template>
-  <ProjectCard />
+  <main class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="mb-5 pb-5 text-center">I miei progetti</h1>
+        </div>
+      </div>
+      <div class="row gy-4">
+        <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+      </div>
+    </div>
+  </main>
 </template>
-<style lang="scss"></style>
+<style lang="scss">
+@import './styles/generals.scss';
+</style>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,10 +1,25 @@
 <script>
 export default {
+    props: {
+        project: Object
+    }
   
 }
 </script>
 <template>
-
+<div class="col-12 col-md-6 col-lg-4">
+    <div class="card">
+        <img :src="project.preview.startsWith('http') ? project.preview : `http://127.0.0.1:8000/storage/${project.preview}`" :alt="project.name" class="card-img-top">
+        <div class="card-title p-3">
+            <h3>{{project.name}} project</h3>
+            <!-- <h5></h5> -->
+        </div>
+        <div class="card-body">
+            <p class="text-end">Data upload: <strong>{{project.date_of_upload}} </strong></p>
+            <p class="card-text">{{project.description}}</p>
+        </div>
+    </div>
+</div>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
 </style>
