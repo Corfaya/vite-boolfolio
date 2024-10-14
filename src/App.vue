f21452475b9de79a304e7df02c964affcf2158dc
Corfaya <lauracorrado0696@gmail.com>
2024-10-14 15:42:17 +0200

    [UPD] Milestone 4 DONE: get axios to receive datas from backend

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,22 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      
+
     }
   },
-    created() {
-      
-    },
-    methods: {
-      
+  created() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+        console.log(res.data.results)
+      })
     }
+  }
 }
 </script>
 <template>
-  
+<h1>Prova</h1>
 </template>
-<style lang="scss">
-  
-</style>
+<style lang="scss"></style>
