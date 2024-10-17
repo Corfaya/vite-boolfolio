17a55c77e95f3aa0a438bac1dec069e60018bedd
Corfaya <lauracorrado0696@gmail.com>
2024-10-17 16:21:56 +0200

    [UPD] first elements for PageContacts.vue

---
+++ src/pages/PageContacts.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="bg-main">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 class="text-center">Contact me!</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <ul class="list-unstyled d-flex">
+                    <li><strong>Address</strong>: Via dei matti, 0</li>
+                    <li><strong>City</strong>: Silent hill</li>
+                    <li><strong>E-mail</strong>: prova@test.it</li>
+                    <li><strong>Phone</strong>: 0000/000000</li>
+                    <li><strong>Pec</strong>: prova@pec.it</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <form method="POST" @submit.prevent="send"></form>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    @import '../styles/app.scss';
+    .bg-main {
+        background-color: $primaryText;
+        h1 {
+            color: $hover;
+            text-shadow: 3px 2px 1px $body;
+        }
+    }
+
+    ul {
+        li{
+            margin-right: 10px;
+        }
+    }
+</style>
