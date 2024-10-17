bcef0d9b8479d14d149bfdba899adc2dd85b2b53
Corfaya <lauracorrado0696@gmail.com>
2024-10-17 17:50:28 +0200

    [UPD] Bonus 1 done

--- src/components/FormContact.vue
+++ src/components/FormContact.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import { store } from '../store';
+import { errorMessages } from 'vue/compiler-sfc';
 export default {
     data() {
         return {
@@ -14,12 +15,16 @@ export default {
             phone: '',
             email_address: '',
             content: '',
+            //bonus 1
+            errors: ''
         }
     },
     methods: {
         send() {
             // bonus 2
             this.sending = true
+            //bonus 1
+            this.errors = {}
 
             const info = {
                 name: this.name,
@@ -29,16 +34,20 @@ export default {
                 content: this.content,
             }
 
+
             axios.post(`${store.url}${store.epContacts}`, info).then((res) => {
                 if (res.data.success) {
                     this.name = '',
-                    this.surname = '',
-                    this.phone = '',
-                    this.email_address= '',
-                    this.content = ''
+                        this.surname = '',
+                        this.phone = '',
+                        this.email_address = '',
+                        this.content = ''
                     //bonus 2
                     this.sending = false
                     this.sentSuccess = true
+                } else {
+                    // bonus 1
+                    this.errors = res.data.errors
                 }
             })
         }
@@ -50,34 +59,58 @@ export default {
         <div class="row">
             <div class="col-12 col-md-6">
                 <label for="name" class="control-label">Name</label>
-                <input v-model="name" type="text" name="name" id="name" placheholder="Your name" class="form-control">
+                <input v-model="name" type="text" name="name" id="name" placheholder="Your name" class="form-control"
+                    :class="errors.name ? 'is-invalid' : ''">
+                <!-- bonus 1 -->
+                <div v-for="(er, i) in errors.name" :key="`error-${i}`" class="alert error">
+                    {{ er }}
+                </div>
             </div>
             <div class="col-12 col-md-6">
                 <label for="surname" class="control-label">Surname</label>
-                <input v-model="surname" type="text" name="surname" id="surname" placheholder="Your surname" class="form-control">
+                <input v-model="surname" type="text" name="surname" id="surname" placheholder="Your surname"
+                    class="form-control" :class="errors.surname ? 'is-invalid' : ''">
+                <!-- bonus 1 -->
+                <div v-for="(er, i) in errors.surname" :key="`error-${i}`" class="alert error">
+                    {{ er }}
+                </div>
             </div>
             <div class="col-12 col-md-6">
                 <label for="phone" class="control-label">Phone number</label>
                 <input v-model="phone" type="text" name="phone" id="phone" placheholder="Phone number here"
-                    class="form-control">
+                    class="form-control" :class="errors.phone ? 'is-invalid' : ''">
+                <!-- bonus 1 -->
+                <div v-for="(er, i) in errors.phone" :key="`error-${i}`" class="alert error">
+                    {{ er }}
+                </div>
             </div>
             <div class="col-12 col-md-6">
                 <label for="email_address" class="control-label">Email</label>
                 <input v-model="email_address" type="email" name="email_address" id="email_address"
-                    placheholder="Your email address" class="form-control">
+                    placheholder="Your email address" class="form-control"
+                    :class="errors.email_address ? 'is-invalid' : ''">
+                <!-- bonus 1 -->
+                <div v-for="(er, i) in errors.email_address" :key="`error-${i}`" class="alert error">
+                    {{ er }}
+                </div>
             </div>
             <div class="col-12">
                 <label for="Content" class="control-label">Your message:</label>
                 <textarea v-model="content" name="content" id="content" class="form-control"
-                    placeholder="Space for your message"></textarea>
+                    :class="errors.content ? 'is-invalid' : ''" placeholder="Space for your message"></textarea>
+                <!-- bonus 1 -->
+                <div v-for="(er, i) in errors.content" :key="`error-${i}`" class="alert error">
+                    {{ er }}
+                </div>
             </div>
             <div class="col-12 d-flex justify-content-center">
                 <!-- with bonus 2: -->
-                <button type="submit" class="py-3 px-4 mt-5 mb-2" :disabled="sending">{{sending ? 'Sending message...' : 'Send'}}</button>
+                <button type="submit" class="py-3 px-4 mt-5 mb-2" :disabled="sending">{{ sending ? 'Sending message...' :
+                    'Send'}}</button>
             </div>
         </div>
     </form>
-    <div class="alert text-center fw-bolder" v-if="sentSuccess">
+    <div class="alert fw-bolder" v-if="sentSuccess">
         Message sent successfully. We'll contact you soon!
     </div>
 </template>
@@ -107,5 +140,10 @@ button {
     color: $hover;
     background-color: $primaryText;
     letter-spacing: 2px;
+    text-align: center;
+    &.error {
+        color: #ff2828;
+        border: 1px solid #ff2828;
+    }
 }
 </style>
