9e7263a70268b0aa6b5cce9061062604dda1c567
Corfaya <lauracorrado0696@gmail.com>
2024-10-18 10:00:53 +0200

    [UPD] maps added

--- src/pages/PageContacts.vue
+++ src/pages/PageContacts.vue
@@ -32,6 +32,7 @@ export default {
                     <li><strong>Phone</strong>: 0000/000000 - </li>
                     <li><strong>Pec</strong>: prova@pec.it</li>
                 </ul>
+                <iframe class="my-4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20728.85340947669!2d-77.47462033862145!3d18.190663524294063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8edbba1b5c17ad65%3A0xbfcdccfe20624462!2sSilent%20Hill%2C%20Giamaica!5e1!3m2!1sit!2sit!4v1729238060166!5m2!1sit!2sit" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
